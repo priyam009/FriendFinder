93ea1f640b7fbd6025d41a220daad52fb29c5366
Priyam <priyam009@live.com>
2019-09-08 21:52:56 +1000

    Add friends API data

---
+++ app/data/friends.js
@@ -0,0 +1,103 @@
+var friends = [
+  {
+    name: "John Doe",
+    photo:
+      "http://placekitten.com/g/200/300",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  {
+    name: "Jacob Deming",
+    photo:
+      "http://placekitten.com/g/200/300",
+    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+  },
+  {
+    name: "Jeremiah Scanlon",
+    photo: "http://placekitten.com/g/200/300",
+    scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+  },
+  {
+    name: "Louis T. Delia",
+    photo:
+      "https://dog.ceo/api/breeds/image/random",
+    scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
+  },
+  {
+    name: "Lou Ritter",
+    photo:
+      "http://placekitten.com/g/200/300",
+    scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+  },
+  {
+    name: "Jordan Biason",
+    photo:
+      "https://dog.ceo/api/breeds/image/random",
+    scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
+  },
+  {
+    name: "Jose Elliott",
+    photo: "https://dog.ceo/api/breeds/image/random",
+    scores: ["5", "4", "2", "3", "1", "3", "3", "3", "2", "4"]
+  },
+  {
+    name: "Emma Mills",
+    photo:
+      "https://www.petmd.com/sites/default/files/petmd-kitten-development.jpg",
+    scores: ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2"]
+  },
+  {
+    name: "Kevin Carroll",
+    photo:
+      "https://dog.ceo/api/breeds/image/random",
+    scores: ["2", "2", "5", "2", "2", "5", "5", "5", "4", "3"]
+  },
+  {
+    name: "Margaret Patterson",
+    photo:
+      "http://placekitten.com/g/200/300",
+    scores: ["3", "5", "4", "2", "4", "5", "5", "4", "1", "1"]
+  },
+  {
+    name: "Julia Lynch",
+    photo: "http://placekitten.com/g/200/300",
+    scores: ["3", "2", "4", "5", "2", "2", "3", "1", "2", "1"]
+  },
+  {
+    name: "Tanner S Bodrero",
+    photo: "http://placekitten.com/g/200/300",
+    scores: ["1", "2", "3", "4", "1", "2", "3", "3", "2", "4"]
+  },
+  {
+    name: "Pamela Mills",
+    photo: "http://placekitten.com/g/200/300",
+    scores: ["2", "2", "2", "3", "3", "3", "3", "3", "3", "2"]
+  },
+  {
+    name: "Kelly Daniels",
+    photo: "http://placekitten.com/g/200/300",
+    scores: ["2", "3", "3", "5", "3", "3", "2", "5", "1", "2"]
+  },
+  {
+    name: "Janice Jimenez",
+    photo: "http://placekitten.com/g/200/300",
+    scores: ["2", "3", "3", "5", "3", "3", "2", "5", "1", "2"]
+  },
+  {
+    name: "Judith Bryant",
+    photo: "http://placekitten.com/g/200/300",
+    scores: ["2", "3", "3", "5", "4", "3", "2", "5", "1", "2"]
+  },
+  {
+    name: "Jonathan Davidson",
+    photo:
+      "https://dog.ceo/api/breeds/image/random",
+    scores: ["5", "3", "3", "2", "4", "1", "5", "3", "3", "4"]
+  },
+  {
+    name: "Robert Miller",
+    photo: "https://dog.ceo/api/breeds/image/random",
+    scores: ["2", "3", "3", "2", "3", "2", "3", "3", "3", "3"]
+  }
+];
+
+module.exports = friends;
